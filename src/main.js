97d2f3d1e98b67507a6c2a9a59fd56381eb34142
Jomit <jomitv@live.com>
2016-01-29 10:20:25 -0800

    React routing redirection, transition and # vs history url scheme configuration

--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ var React = require('react');
 var Router = require('react-router');
 var routes = require('./routes');
 
+
 Router.run(routes, function(Handler){
 	React.render(<Handler/>, document.getElementById("app"));
 });
+
+// Using the History Location based url for routing instead of # url.
+// Does not work on browser < IE10
+//Router.run(routes, Router.HistoryLocation, function(Handler){
+//	React.render(<Handler/>, document.getElementById("app"));
+//});
